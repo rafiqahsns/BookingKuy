db4a3ab927b3a28e969072bb99a33ac3695ac009
rafiqahsns <rafiqahsns@yahoo.com>
2018-05-02 11:38:11 +0700

    front end tambahan

--- src/pages/ruangan/ruangan.ts
+++ src/pages/ruangan/ruangan.ts
@@ -14,12 +14,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'ruangan.html',
 })
 export class RuanganPage {
+  items = [
+    'RK U 2.01',
+    'RK U 2.02',
+    'RK U 3.01'
+  ];
 
+  faperta = [
+    'RK PINUS 1',
+    'RK PINUS 2',
+    'RK 16 FAC 401 D'
+  ];
+
+  itemSelected(item: string) {
+    console.log("Selected Item", item);
+  }
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RuanganPage');
   }
-
+ 
 }
