549510c1c96b3a5bb51244446f8f8f46c92aa316
dikmaj <dikmajlov@gmail.com>
2018-05-07 23:17:27 +0700

    Add files via upload

---
+++ src/pages/info-ruangan/info-ruangan.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service/auth-service';
+import { RuanganPage } from '../ruangan/ruangan';
+
+/**
+ * Generated class for the InfoRuanganPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-info-ruangan',
+  templateUrl: 'info-ruangan.html',
+})
+export class InfoRuanganPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    const view = JSON.parse(localStorage.getItem('userView'));
+    console.log(view);
+    this.userDetails = view.userData;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad InfoRuanganPage');
+  }
+
+}
