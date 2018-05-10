00090b6ca5b077e185610e88374d921cfd352434
dikmaj <dikmajlov@gmail.com>
2018-05-10 17:07:45 +0700

    update minor

--- src/pages/waktu-next/waktu-next.ts
+++ src/pages/waktu-next/waktu-next.ts
@@ -15,7 +15,11 @@ import { DetailPinjamPage } from '../detail-pinjam/detail-pinjam';
   templateUrl: 'waktu-next.html',
 })
 export class WaktuNextPage {
-
+  time = [false, false, false];
+  timeDetails = ['14.00-15.00', '16.00-17.00', '21.00-22.00'];
+  timeFix = ['','',''];
+  date: any;
+  detailPinjam = {"date": '', "time": ''};
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -23,6 +27,21 @@ export class WaktuNextPage {
     console.log('ionViewDidLoad WaktuNextPage');
   }
   nextdetail(){
+    var i;
+    for(i=0; i < this.time.length; i++){
+      if(this.time[i]){
+        this.timeFix[i] = this.timeDetails[i];
+      }
+    }
+    this.detailPinjam = {"date": this.date, "time": this.timeFix};
+    //console.log(this.detailPinjam);
+    localStorage.setItem('pinjamDetails', JSON.stringify(this.detailPinjam));
     this.navCtrl.push(DetailPinjamPage);
+  }
+  updatetime(){
+    //console.log(this.time);
+  }
+  updatedate(){
+    //console.log(this.date);
   }
 }
