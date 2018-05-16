218844fdf7c278150e784b29906e336477ae4b26
idzhar <idzharbae@gmail.com>
2018-05-16 10:52:28 +0700

    Fix picture sanitizer in akunpage and settingpage

--- src/pages/akun/akun.ts
+++ src/pages/akun/akun.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, App, AlertController } from 'ionic
 import { SettingsPage } from '../settings/settings';
 import { TabsPage } from '../tabs/tabs';
 import { PhotoPage } from '../photo/photo';
+import { DomSanitizer } from '@angular/platform-browser';
 
 /**
  * Generated class for the AkunPage page.
@@ -17,9 +18,12 @@ import { PhotoPage } from '../photo/photo';
   templateUrl: 'akun.html',
 })
 export class AkunPage {
-
+  public userDetails;
   constructor(public navCtrl: NavController, public navParams: NavParams, public appCtrl:App,
-    private alertCtrl:AlertController) {
+    private alertCtrl:AlertController, private sanitizer: DomSanitizer) {
+      const data = JSON.parse(localStorage.getItem('userData'));
+     this.userDetails = data.userData;
+     this.userDetails.profilpic = this.sanitizer.bypassSecurityTrustUrl('data:image/png;base64,'+this.userDetails.profilpic);
   }
   public showPass = false;
   public type = 'password';
