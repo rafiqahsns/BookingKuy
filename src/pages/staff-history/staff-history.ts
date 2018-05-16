d4d643c1d50be28410ebda5dbe21f741f7078940
idzhar <idzharbae@gmail.com>
2018-05-16 09:50:39 +0700

    Fix refresh() error

--- src/pages/staff-history/staff-history.ts
+++ src/pages/staff-history/staff-history.ts
@@ -121,76 +121,84 @@ export class StaffHistoryPage {
     }else{ list.show = true;}
   }
   refresh(){
-    this.authService.postData(this.userData,'history').then((result) => {
-    this.responseData = result;
-    if(this.responseData.hasil){
-      console.log(this.responseData);
-      this.log = [];
-      var i;
-      for(i=0 ; i < this.responseData.hasil.length; i++){
-        this.log[i] = this.responseData.hasil[i];
+  const data = JSON.parse(localStorage.getItem('userData'));
+  this.userDetails = data.userData;
+  this.userData.penjaga = this.userDetails.name;
+  //this.date = moment().format('YYYY-MM-DD HH:mm:ss');
+  //this.date = moment(this.date).format('dddd');
+  //this.time = Number(moment().format('HHmmss'));
+  //console.log(this.date);
+  //console.log(this.time);
+  this.authService.postData(this.userData,'history').then((result) => {
+  this.responseData = result;
+  if(this.responseData.hasil){
+    console.log(this.responseData);
 
-        this.Temp = this.log[i].history_date.split(" ");
-        //this.day = moment(this.log[i].history_date).format("dddd");
-        this.date = this.Temp[0].split("-");
-        //this.time = this.Temp[1];
-        //console.log(this.day);
-        //console.log(this.date);
-        //console.log(this.time);
-        this.log[i].history_date = this.date[2] + "-" + this.date[1] + "-" + this.date[0] + " " + this.Temp[1];
+    var i;
+    for(i=0 ; i < this.responseData.hasil.length; i++){
+      this.log[i] = this.responseData.hasil[i];
+      this.log[i].show = true;
+      this.Temp = this.log[i].history_date.split(" ");
+      //this.day = moment(this.log[i].history_date).format("dddd");
+      this.date = this.Temp[0].split("-");
+      //console.log(this.Temp[0]);
+      //console.log(this.date);
+      //console.log(this.time);
+      this.log[i].history_date = this.date[2] + "-" + this.date[1] + "-" + this.date[0] + " " + this.Temp[1];
+      //console.log(this.day);
 
-        this.Temp = this.log[i].tanggal.split("-").join();
-        this.datePinjam = Number(this.Temp.replace(/\,/g,''));
+      this.Temp = this.log[i].tanggal.split("-").join();
+      this.datePinjam = Number(this.Temp.replace(/\,/g,''));
 
-        this.Temp = this.log[i].waktu.split("-").join();
-        this.timePinjam = this.Temp[0].split(":").join();
-        this.timeSelesai = this.Temp[1].split(":").join();
-        this.timePinjam += ",00";
-        this.timeSelesai += ",00";
-        this.timePinjam = Number(this.timePinjam.replace(/\,/g, ''));
-        this.timeSelesai = Number(this.timeSelesai.replace(/\,/g, ''));
+      this.Temp = this.log[i].waktu.split("-").join();
+      this.timePinjam = this.Temp[0].split(":").join();
+      this.timeSelesai = this.Temp[1].split(":").join();
+      this.timePinjam += ",00";
+      this.timeSelesai += ",00";
+      this.timePinjam = Number(this.timePinjam.replace(/\,/g, ''));
+      this.timeSelesai = Number(this.timeSelesai.replace(/\,/g, ''));
 
-        this.dateNow = Number(moment().format('YYYYMMDD'));
-        if(this.dateNow > this.datePinjam){
-            if(this.log[i].status == "0"){
-              this.log[i].status = "Ruangan tidak dikonfirmasi, permintaan dibatalkan";
-            } else{
-              this.log[i].status = "Peminjaman ruangan telah berakhir";
-            }
-
-        } else if (this.dateNow == this.datePinjam){
-            this.timeNow = Number(moment().format('HHmmss'));
-            if(this.timeNow >= this.timePinjam && this.timeNow <= this.timeSelesai){
-                if(this.log[i].status == "0"){
-                  this.log[i].status = "Ruangan tidak dikonfirmasi, permintaan dibatalkan";
-                } else{
-                  this.log[i].status = "Ruangan sedang dipakai";
-                }
-            } else if (this.timeNow < this.timePinjam){
-              if(this.log[i].status == "0"){
-                this.log[i].status = "Menunggu konfirmasi";
-              } else{
-                this.log[i].status = "Ruangan telah dikonfirmasi";
-              }
-            } else if( this.timeNow > this.timeSelesai){
-                if(this.log[i].status == "0"){
-                  this.log[i].status = "Ruangan tidak dikonfirmasi, permintaan dibatalkan";
-                } else{
-                  this.log[i].status = "Peminjaman ruangan telah berakhir";
-                }
-            }
-        } else{
+      this.dateNow = Number(moment().format('YYYYMMDD'));
+      if(this.dateNow > this.datePinjam){
           if(this.log[i].status == "0"){
-            this.log[i].status = "Menunggu konfirmasi";
+            this.log[i].status = "Ruangan tidak dikonfirmasi, permintaan dibatalkan";
           } else{
-            this.log[i].status = "Ruangan telah dikonfirmasi";
+            this.log[i].status = "Peminjaman ruangan telah berakhir";
           }
+
+      } else if (this.dateNow == this.datePinjam){
+          this.timeNow = Number(moment().format('HHmmss'));
+          if(this.timeNow >= this.timePinjam && this.timeNow <= this.timeSelesai){
+              if(this.log[i].status == "0"){
+                this.log[i].status = "Ruangan tidak dikonfirmasi, permintaan dibatalkan";
+              } else{
+                this.log[i].status = "Ruangan sedang dipakai";
+              }
+          } else if (this.timeNow < this.timePinjam){
+            if(this.log[i].status == "0"){
+              this.log[i].status = "Menunggu konfirmasi";
+            } else{
+              this.log[i].status = "Ruangan telah dikonfirmasi";
+            }
+          } else if( this.timeNow > this.timeSelesai){
+              if(this.log[i].status == "0"){
+                this.log[i].status = "Ruangan tidak dikonfirmasi, permintaan dibatalkan";
+              } else{
+                this.log[i].status = "Peminjaman ruangan telah berakhir";
+              }
+          }
+      } else{
+        if(this.log[i].status == "0"){
+          this.log[i].status = "Menunggu konfirmasi";
+        } else{
+          this.log[i].status = "Ruangan telah dikonfirmasi";
+              }
+            }
         }
-    }
-  }
-    }, (err) => {
-      // Error log
-    });
-  }
+      }
+        }, (err) => {
+          // Error log
+        });
+      }
 
 }
